e066a0971cb433f707b4d782f97643804e34a755
YesuBabuPujala <86478609+YesuBabuPujala@users.noreply.github.com>
2021-10-29 16:30:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,19 +66,19 @@ function draw() {
   //   text("Docking Successful!", 200, 300);
   // }
 
+  // if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
+  //  hasDocked = true;
+  // textSize(25);
+  // fill("white")
+  //   text("Docking UnSuccessful!", 200, 300);
+  // }
+
    if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
      hasDocked = true;
      textSize(25);
      fill("white")
-     text("Docking UnSuccessful!", 200, 300);
+     text("Docking Successful!", 200, 300);
    }
-
-  // if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
-  //   hasDocked = true;
-  //   textSize(25);
-  //   fill("white")
-  //   text("Docking Successful!", 200, 300);
-  // }
 
   drawSprites();
 }
